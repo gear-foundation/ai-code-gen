6173755f2a56322e63912ef5cec9db7f8452d2d2
Eugene Kotsarev <gimmethenick@gmail.com>
2025-06-09 20:59:14 +0300

    test without dockerfile envs

--- frontend/src/app/hooks/useEnableWeb3.ts
+++ frontend/src/app/hooks/useEnableWeb3.ts
@@ -1,28 +1,28 @@
-import { useEffect, useState } from "react";
-import { useAccount } from "@gear-js/react-hooks";
-import { name as appName } from '../../../package.json';
-import { web3Enable } from "@polkadot/extension-dapp";
+import { useEffect, useState } from "react"
+import { useAccount } from "@gear-js/react-hooks"
+import { web3Enable } from "@polkadot/extension-dapp"
 
+import { name as appName } from "../../../package.json"
 
 export const useEnableWeb3 = () => {
-    const { wallets } = useAccount();
-    const [isEnable, setIsEnable] = useState(false);
-    const [web3IsEnable, setWeb3IsEnable] = useState(false);
-  
-    useEffect(() => {
-      if (!wallets) return;
-      if (isEnable) return;
-  
-      const syncWeb3 = async () => {
-        await web3Enable(appName);
-        setWeb3IsEnable(true);
-        setIsEnable(true);
-      }
-  
-      syncWeb3();
-    }, [wallets]);
-  
-    return {
-      web3IsEnable
-    };
+  const { wallets } = useAccount()
+  const [isEnable, setIsEnable] = useState(false)
+  const [web3IsEnable, setWeb3IsEnable] = useState(false)
+
+  useEffect(() => {
+    if (!wallets) return
+    if (isEnable) return
+
+    const syncWeb3 = async () => {
+      await web3Enable(appName)
+      setWeb3IsEnable(true)
+      setIsEnable(true)
+    }
+
+    syncWeb3()
+  }, [wallets])
+
+  return {
+    web3IsEnable,
   }
+}
