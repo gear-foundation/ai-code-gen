2cb55c39ec73adfc3c719572d0d4f5b075ef7b9c
sergey filyanin <filianin.sergey@gmail.com>
2025-06-09 18:58:48 +0200

    Update index.py

--- backend/index.py
+++ backend/index.py
@@ -48,4 +48,4 @@ def create_app(config_name="development"):
 app = create_app(config_name=os.getenv("FLASK_ENV", "development"))
 
 if __name__ == "__main__":
-    app.run(debug=app.config["DEBUG"])
+    app.run(host="0.0.0.0", port=5000, debug=app.config["DEBUG"])
